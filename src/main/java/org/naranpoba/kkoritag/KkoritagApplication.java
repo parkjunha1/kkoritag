552c3b846b6acec26b9ac0ab67531d1887002678
pjh <hateme18@naver.com>
2024-06-05 00:12:23 +0900

    깃허브 올리기 연습

---
+++ src/main/java/org/naranpoba/kkoritag/KkoritagApplication.java
@@ -0,0 +1,13 @@
+package org.naranpoba.kkoritag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkoritagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkoritagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/naranpoba/kkoritag/KkoritagApplicationTests.java
@@ -0,0 +1,13 @@
+package org.naranpoba.kkoritag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkoritagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
